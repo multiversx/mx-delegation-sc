5d1a2599c748a9045d559465c7931be9a38c4c13
andrei-marinica <andrei_m_marinica@yahoo.com>
2020-03-16 04:19:33 +0200

    staking contract mock

---
+++ staking-mock/src/lib.rs
@@ -0,0 +1,21 @@
+
+#![no_std]
+#![no_main]
+#![allow(non_snake_case)]
+#![allow(unused_attributes)]
+
+
+// global contract variables
+static STAKE_KEY: [u8; 32] = [0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+
+#[elrond_wasm_derive::contract(DelegationImpl)]
+pub trait StakingMock {
+
+    fn init(&self) {
+    }
+
+    #[payable(payment)]
+    fn stake(&self, payment: &BigInt) {
+        self.storage_store_big_int(&STAKE_KEY.into(), payment);
+    }
+}
