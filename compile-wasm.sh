9d686ee3df57f6a93b264c3afd486c16f512eed5
andrei-marinica <andrei_m_marinica@yahoo.com>
2020-04-03 12:30:34 +0300

    changed bin names

--- compile-wasm.sh
+++ compile-wasm.sh
@@ -2,13 +2,13 @@
 
 # script provided for convenience, to build and extract wasm output to root
 
-cargo build --bin wasm --target=wasm32-unknown-unknown --release
-mv target/wasm32-unknown-unknown/release/wasm.wasm delegation.wasm
+cargo build --bin delegation --target=wasm32-unknown-unknown --release
+mv target/wasm32-unknown-unknown/release/delegation.wasm delegation.wasm
 wasm-snip delegation.wasm -o delegation.wasm --snip-rust-fmt-code --snip-rust-panicking-code
 #wasm-gc delegation.wasm
 # twiggy top -n 100 delegation.wasm > twiggy-snip.txt
 
 cd auction-mock
-cargo build --bin wasm --target=wasm32-unknown-unknown --release
+cargo build --bin auction-mock --target=wasm32-unknown-unknown --release
 cd ..
-mv auction-mock/target/wasm32-unknown-unknown/release/wasm.wasm auction-mock.wasm
+mv auction-mock/target/wasm32-unknown-unknown/release/auction-mock.wasm auction-mock.wasm
