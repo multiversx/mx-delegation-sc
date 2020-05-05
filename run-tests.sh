d0a79062d4d36eee19fcbb742ad4ad1464959b65
andrei-marinica <andrei.marinica@elrond.com>
2020-05-05 19:28:38 +0300

    updated tests

---
+++ run-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Works with the latest version of Arwen cloned in the default location
+# Will be replaced by erdpy
+$GOPATH/src/github.com/ElrondNetwork/arwen-wasm-vm/cmd/test/test tests
