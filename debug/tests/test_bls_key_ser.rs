2be1650ddcdb047195b050291cf17e2fcbc6056b
andrei-marinica <andrei.marinica@elrond.com>
2020-06-08 22:05:52 +0300

    fixed bls serialization tests

--- debug/tests/test_bls_key_ser.rs
+++ debug/tests/test_bls_key_ser.rs
@@ -1,24 +1,24 @@
 
-use elrond_wasm::serializer::{to_bytes, from_bytes};
+use elrond_wasm::esd_light::*;
 use sc_delegation_rs::bls_key::*;
 use elrond_wasm::Vec;
 
 #[test]
-fn test_bls_serde() {
+fn test_bls_serialization() {
     let bls_key = BLSKey([4u8; BLS_KEY_BYTE_LENGTH]);
     let expected_bytes: &[u8] = &[4u8; BLS_KEY_BYTE_LENGTH];
 
     // serialize
-    let serialized_bytes = to_bytes(&bls_key).unwrap();
+    let serialized_bytes = bls_key.top_encode();
     assert_eq!(serialized_bytes.as_slice(), expected_bytes);
 
     // deserialize
-    let deserialized: BLSKey = from_bytes(serialized_bytes.as_slice()).unwrap();
+    let deserialized: BLSKey = decode_from_byte_slice(&serialized_bytes[..]).unwrap();
     assert_eq!(deserialized.to_vec(), bls_key.to_vec());
 }
 
 #[test]
-fn test_vec_bls_serde() {
+fn test_vec_bls_serialization() {
     let mut bls_vec: Vec<BLSKey> = Vec::new();
     for _ in 0..3 {
         bls_vec.push(BLSKey([4u8; BLS_KEY_BYTE_LENGTH]));
@@ -26,11 +26,11 @@ fn test_vec_bls_serde() {
     let expected_bytes: &[u8] = &[4u8; BLS_KEY_BYTE_LENGTH*3];
 
     // serialize
-    let serialized_bytes = to_bytes(&bls_vec).unwrap();
+    let serialized_bytes = bls_vec.top_encode();
     assert_eq!(serialized_bytes.as_slice(), expected_bytes);
 
     // deserialize
-    let deserialized: Vec<BLSKey> = from_bytes(serialized_bytes.as_slice()).unwrap();
+    let deserialized: Vec<BLSKey> = decode_from_byte_slice(serialized_bytes.as_slice()).unwrap();
     assert_eq!(deserialized.len(), bls_vec.len());
     for i in 0..3 {
         assert_eq!(deserialized[i].to_vec(), bls_vec[i].to_vec());
