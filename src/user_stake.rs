37e95a498fee95167290f7456df9b28a2bd2885d
andrei-marinica <andrei.marinica@elrond.com>
2020-06-09 13:16:13 +0300

    withdraw inactive stake fix + test

--- src/user_stake.rs
+++ src/user_stake.rs
@@ -89,10 +89,10 @@ pub trait UserStakeModule {
         let withdraw_stake = self.user_data()._get_user_stake_of_type(user_id, UserStakeState::WithdrawOnly);
         if &amount <= &withdraw_stake {
             // first withdraw from unavailable inactive stake
-            self.user_data()._decrease_user_stake_of_type(user_id, UserStakeState::WithdrawOnly, &withdraw_stake);
+            self.user_data()._decrease_user_stake_of_type(user_id, UserStakeState::WithdrawOnly, &amount);
         } else {
+            self.user_data()._decrease_user_stake_of_type(user_id, UserStakeState::WithdrawOnly, &withdraw_stake);
             let remaining = &amount - &withdraw_stake;
-            // let inactive_stake = self.user_data()._get_user_stake_of_type(user_id, UserStakeState::Inactive);
             let enough = self.user_data()._decrease_user_stake_of_type(user_id, UserStakeState::Inactive, &remaining);
             if !enough {
                 return Err("cannot withdraw more than inactive stake");
