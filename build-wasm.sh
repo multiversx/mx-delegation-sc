e58c09959948a5d7c980ae2101e696f93ab89dcd
andrei-marinica <andrei.marinica@elrond.com>
2021-06-09 18:15:12 +0300

    v0.5.7 build

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,8 +4,8 @@ DELEGATION_DIR=$(dirname "$0")
 erdpy --verbose contract build "$DELEGATION_DIR/latest_full"
 erdpy --verbose contract build "$DELEGATION_DIR/latest_update"
 
-cp latest_full/output/delegation_latest_full.wasm v0_5_6_full/output/delegation_v0_5_6_full.wasm
-cp latest_update/output/delegation_latest_update.wasm v0_5_6_update/output/delegation_v0_5_6_update.wasm
+cp latest_full/output/delegation_latest_full.wasm v0_5_7_full/output/delegation_v0_5_7_full.wasm
+cp latest_update/output/delegation_latest_update.wasm v0_5_7_update/output/delegation_v0_5_7_update.wasm
 
 ## For playing around without erdpy:
 
