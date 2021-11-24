d3493ab8aedf3b7b28b213b9106a04587b3d1f31
Andrei Marinica <andrei.marinica@elrond.com>
2021-11-24 15:14:44 +0200

    cleanup

--- latest/src/events.rs
+++ latest/src/events.rs
@@ -3,39 +3,30 @@ elrond_wasm::imports!();
 /// Contains all events that can be emitted by the contract.
 #[elrond_wasm::derive::module]
 pub trait EventsModule {
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000001")]
     #[event("userStake")]
     fn stake_event(&self, #[indexed] delegator: &ManagedAddress, amount: &BigUint);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000002")]
     #[event("userUnstake")]
     fn unstake_event(&self, #[indexed] delegator: &ManagedAddress, amount: &BigUint);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000003")]
     #[event("nodeStakeOk")]
     fn stake_node_ok_event(&self);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000004")]
     #[event("nodeStakeFail")]
     fn stake_node_fail_event(&self, reason: &[u8]);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000005")]
     #[event("nodeUnstakeOk")]
     fn unstake_node_ok_event(&self);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000006")]
     #[event("nodeUnstakeFail")]
     fn unstake_node_fail_event(&self, reason: &[u8]);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000007")]
     #[event("nodeUnbondOk")]
     fn unbond_node_ok_event(&self);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000008")]
     #[event("nodeUnbondFail")]
     fn unbond_node_fail_event(&self, reason: &[u8]);
 
-    // #[legacy_event("0x0000000000000000000000000000000000000000000000000000000000000009")]
     #[event("userClaimRewards")]
     fn claim_rewards_event(&self, #[indexed] user: &ManagedAddress, amount: &BigUint);
 }
