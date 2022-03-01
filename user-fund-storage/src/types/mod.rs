accc5777db513745b812ccef5da7519fa771b6ca
Andrei Marinica <andrei.marinica@elrond.com>
2022-03-01 21:08:14 +0200

    remove alloc - affected users

--- user-fund-storage/src/types/mod.rs
+++ user-fund-storage/src/types/mod.rs
@@ -1,7 +1,9 @@
+mod affected_user_id_vec;
 mod fund_item;
 mod fund_list_info;
 mod fund_type;
 
+pub use affected_user_id_vec::*;
 pub use fund_item::FundItem;
 pub use fund_list_info::FundsListInfo;
 pub use fund_type::{FundDescription, FundType};
