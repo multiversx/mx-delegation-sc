039d1a7e3c52b8f05eb36a89790d4f9d15e2d353
Andrei Marinica <andrei.marinica@multiversx.com>
2024-05-08 10:51:13 +0300

    interactor: num users, user stake

--- interact/src/deleg_interact.rs
+++ interact/src/deleg_interact.rs
@@ -19,6 +19,14 @@ async fn main() {
         InteractCliCommand::Global => {
             basic_interact.query_global().await;
         }
+        InteractCliCommand::UserFull => {
+            basic_interact.query_all_user_stake_by_type().await;
+        }
+        InteractCliCommand::NumUsers => basic_interact.query_num_users().await,
+        InteractCliCommand::UserStake(args) => {
+            let address = Bech32Address::from_bech32_string(args.address.clone());
+            basic_interact.query_user_stake_by_type(&address).await;
+        }
     }
 }
 
@@ -59,6 +67,69 @@ impl AdderInteract {
         println!("Active:          {}", display_egld_amount(&tuple.2));
         println!("UnStaked:        {}", display_egld_amount(&tuple.3));
         println!("DeferredPayment: {}", display_egld_amount(&tuple.4));
+    }
+
+    async fn query_num_users(&mut self) {
+        let num_users = self
+            .interactor
+            .query()
+            .to(&self.config.sc_address)
+            .typed(latest_proxy::DelegationFullProxy)
+            .get_num_users()
+            .returns(ReturnsResult)
+            .prepare_async()
+            .run()
+            .await;
+
+        println!("{num_users}");
+    }
+
+    async fn query_user_stake_by_type(&mut self, address: &Bech32Address) {
+        let result = self
+            .interactor
+            .query()
+            .to(&self.config.sc_address)
+            .typed(latest_proxy::DelegationFullProxy)
+            .get_user_stake_by_type_endpoint(address)
+            .returns(ReturnsResult)
+            .prepare_async()
+            .run()
+            .await;
+
+        let tuple = result.into_tuple();
+
+        println!("WithdrawOnly:    {}", display_egld_amount(&tuple.0));
+        println!("Waiting:         {}", display_egld_amount(&tuple.1));
+        println!("Active:          {}", display_egld_amount(&tuple.2));
+        println!("UnStaked:        {}", display_egld_amount(&tuple.3));
+        println!("DeferredPayment: {}", display_egld_amount(&tuple.4));
+    }
+
+    async fn query_all_user_stake_by_type(&mut self) {
+        let result = self
+            .interactor
+            .query()
+            .to(&self.config.sc_address)
+            .typed(latest_proxy::DelegationFullProxy)
+            .get_all_user_stake_by_type()
+            .returns(ReturnsResult)
+            .prepare_async()
+            .run()
+            .await;
+
+        for user_stake_tuple in result {
+            let tuple = user_stake_tuple.into_tuple();
+            let address = Bech32Address::from(tuple.0.to_address());
+            let stake_by_type = tuple.1.into_tuple();
+
+            print!("{address}: ");
+            print!(" {}", display_egld_amount(&stake_by_type.0));
+            print!(" {}", display_egld_amount(&stake_by_type.1));
+            print!(" {}", display_egld_amount(&stake_by_type.2));
+            print!(" {}", display_egld_amount(&stake_by_type.3));
+            print!(" {}", display_egld_amount(&stake_by_type.4));
+            println!();
+        }
     }
 }
 
--- interact/src/deleg_interact_cli.rs
+++ interact/src/deleg_interact_cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 /// Adder Interact CLI
 #[derive(Default, PartialEq, Eq, Debug, Parser)]
@@ -14,4 +14,20 @@ pub struct InteractCli {
 pub enum InteractCliCommand {
     #[command(name = "global", about = "Query global state")]
     Global,
+
+    #[command(name = "user-full", about = "All user stake by type")]
+    UserFull,
+
+    #[command(name = "num-users", about = "User address list")]
+    NumUsers,
+
+    #[command(name = "user-stake", about = "User stake by type")]
+    UserStake(UserStakeArgs),
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Debug, Args)]
+pub struct UserStakeArgs {
+    /// The user bech32 address
+    #[arg()]
+    pub address: String,
 }
